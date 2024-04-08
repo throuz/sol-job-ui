edc4e2ecbe970013c5f2939f71d6e5300178d12c
HSU JUI SHENG <zed2786328@gmail.com>
2024-04-08 19:42:23 +0800

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
@@ -8,12 +9,24 @@ import {
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
-import Content from "./components/Content";
-import Header from "./components/Header";
 import { GlobalProvider } from "./context/GlobalContext";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import Layout from "./components/Layout";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
 
 function App() {
   const network = WalletAdapterNetwork.Devnet;
@@ -30,8 +43,13 @@ function App() {
               limit={1}
               style={{ fontSize: "20px" }}
             />
-            <Header />
-            <Content />
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="*" element={<NoMatch />} />
+              </Route>
+            </Routes>
           </WalletModalProvider>
         </WalletProvider>
       </ConnectionProvider>
